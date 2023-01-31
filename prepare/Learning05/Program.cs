3f0c668dd9610328fd6819756be213a946609a5b
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-31 16:39:43 -0700

    Completed Week05 Preparation Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> ShapeList = new List<Shape>();
+        ShapeList.Add(new Square("Blue", 5));
+        ShapeList.Add(new Rectangle("Red", 2, 6));
+        ShapeList.Add(new Circle("Yellow", 10));
+        foreach (Shape CurrentShape in ShapeList) {
+            Console.WriteLine(CurrentShape.GetColor());
+            Console.WriteLine($"{CurrentShape.GetArea()}\n");
+        }
     }
 }
